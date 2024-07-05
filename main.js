797e535ccf045ed9ab8cc3f4978a71246e5bb3ef
G-Aleixo <g.aleixo@escolar.ifrn.edu.br>
2024-07-05 17:48:43 -0300

    Better comment and change var to let

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function escolha(opcao) {
-    var conteudo = document.getElementById("conteudo");
-    var resultado = document.getElementById("resultado");
+    let conteudo = document.getElementById("conteudo");
+    let resultado = document.getElementById("resultado");
 
     try {
         conteudo.innerHTML = choices_data[opcao];
     } catch (error) {
-        conteudo.innerHTML = "<h1>Opção inválida!</h1>"; // Não funciona D:
+        // TODO: Não chega aqui
+        conteudo.innerHTML = "<h1>Opção inválida!</h1>";
         resultado.innerHTML = "<p>Opção " + opcao + " não encontrada</p>";
     }
     
