d76e2b2371c6148e340c6e9688e766ab777392e1
BrasilicioHenrique <brasiliciohcr@gmail.com>
2024-07-15 17:14:32 -0300

    opção de voltar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-// Função para fazer uma escolha
 function escolha(opcao) {
     let conteudo = document.getElementById("conteudo");
     let resultado = document.getElementById("resultado");
 
     try {
-        // Salva o estado atual no histórico
+        conteudo.innerHTML = construir_html(opcao)
+    } catch (error) {
+        conteudo.innerHTML = "<h1>Opção inválida!</h1>";
+        resultado.innerHTML = "<p>Opção " + opcao + " não encontrada</p>";
+        console.error(error);
+    }
+    
+    try {
         history.pushState({
             conteudo: conteudo.innerHTML,
             resultado: resultado.innerHTML
         }, '', '');
 
-        // Atualiza o conteúdo com base na escolha
         conteudo.innerHTML = construir_html(opcao);
     } catch (error) {
         conteudo.innerHTML = "<h1>Opção inválida!</h1>";
@@ -19,37 +24,33 @@ function escolha(opcao) {
     }
 }
 
-// Função para construir o HTML da escolha
 function construir_html(opcao) {
     console.log(opcao);
-    let html = "";
+    let html = ""
 
-    if (choices_data[opcao]) {
-        html += <p>${choices_data[opcao]["conteudo"]}</p>;
+    html += `<p>${choices_data[opcao]["conteudo"]}</p>`
 
-        const botoes = choices_data[opcao]["botoes"];
+    const botoes = choices_data[opcao]["botoes"]
 
-        if (botoes && botoes.length > 0) {
-            botoes.forEach((botao, index) => {
-                html += `
-                    <button onclick="escolha('${botao[0]}')">
-                        ${botao[1]}
-                    </button><br>
-                `;
-            });
-        }
+    if (!(botoes === undefined || botoes.length == 0)) {
+        botoes.forEach((botao, index) => {
+            html += `
+                <button onclick="escolha('${botao[0]}')">
+                    ${botao[1]}
+                </button><br>
+            `;
+        });
     }
 
-    return html;
+    return html
+
 }
 
-// Função para lidar com o evento de navegação do navegador
 window.onpopstate = function(event) {
     if (event.state) {
         document.getElementById('conteudo').innerHTML = event.state.conteudo;
         document.getElementById('resultado').innerHTML = event.state.resultado;
     } else {
-        // Caso não haja estado, pode-se exibir uma mensagem ou voltar ao estado inicial
         alert('Não há mais estados para voltar.');
     }
 };
