362fa897bd28e771aef953e63404738220b84b36
BrasilicioHenrique <brasiliciohcr@gmail.com>
2024-07-15 12:36:10 -0300

    mudança geral

--- js/choices.js
+++ js/choices.js
@@ -4,7 +4,7 @@ let choices_data = {
         que você está diferente; não reconhecendo a si mesmo. Olhando para si mesmo, percebe sua diferente aparência. Uma 
         cor completamente escura, não conseguindo identificar nada no seu corpo. Seus olhos arregalam com o pensamento 
         claro: "Eu não sou mais eu. O que eu sou? O que aconteceu comigo?"</p>
-        <img style="height: auto; width: 300px;" src="../img/gregorp.gif" alt="">`,
+        <img style="height: auto; width: 256px;" src="../img/gregorp.gif" alt="">`,
         "botoes": [
             ["olharquarto1", "Olhar ao redor do quarto"],
             ["olharjanela1", "Olhar para a janela"],
@@ -13,14 +13,16 @@ let choices_data = {
     },
     'olharquarto1': {
         "conteudo": `Nota que não é um sonho, o seu quarto está exatamente como de costume, dando para observar até mesmo 
-        a moldura de um recorte de uma revista com uma bela vista, feita por si mesmo.`,
+        a moldura de um recorte de uma revista com uma bela vista, feita por si mesmo.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp.gif" alt="">`,
         "botoes": [
             ["olharjanela1", "Olhar para a janela"],
             ["irdormir1", "Voltar a dormir"]
         ]
     },
     'olharjanela1': {
-        "conteudo": `Ao se levantar da cama e ir para a janela, vê o céu fechado, com várias nuvens e respingos de chuva.`,
+        "conteudo": `Ao se levantar da cama e ir para a janela, vê o céu fechado, com várias nuvens e respingos de chuva.</p>
+        <img style="height: auto; width: 256px;" src="../img/janela.png" alt="">`,
         "botoes": [
             ["olharquarto1", "Olhar ao redor do quarto"],
             ["irdormir1", "Voltar a dormir"]
@@ -34,7 +36,8 @@ let choices_data = {
         escritório, e ainda por cima há o desconforto de andar sempre preocupado com as ligações dos trens, com a cama e 
         com as refeições irregulares. Diabos levem tudo isto! Mas eu não posso reclamar muito; se meu chefe descobrir isso, 
         serei despedido na hora... mas se bem que ser despedido desse trabalho de merda não seria tão ruim. Mas não faço 
-        isso só por causa dos meus pais.`,
+        isso só por causa dos meus pais.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_cama.png" alt="">`,
         "botoes": [
             ["continuar1", "Continuar"]
         ]
@@ -44,7 +47,9 @@ let choices_data = {
         lá o quanto antes. 
         <br> Ao olhar ao relógio que fica em cima da cômoda, nota que já são 6h30. O relógio deveria tocar às quatro da 
         manhã. Todavia, agora já era. O que fazer? O próximo trem passa às sete horas; mas para chegar a tempo, deveria 
-        correr e muito. Mesmo que você consiga pegar o trem, ouvirá muitas reclamações do seu chefe.`,
+        correr e muito. Mesmo que você consiga pegar o trem, ouvirá muitas reclamações do seu chefe.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_cama.png" alt="">
+        <img style="height: auto; width: 256px;" src="../img/relogio.gif" alt="">`,
         "botoes": [
             ["ficarcasa1", "Ficar deitado"],
             ["irtrem1", "Sair correndo para a estação de trem"],
@@ -53,7 +58,8 @@ let choices_data = {
     'irtrem1': {
         "conteudo": `Você rapidamente se levanta e, sem comer nada, sai do quarto correndo e sai de casa. Ninguém da sua 
         casa vê você saindo. Estando na rua, você percebe que muitas pessoas na rua te olham estranho, ficam susurrando 
-        coisas que você não entende.`,
+        coisas que você não entende.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_rua.png" alt="">`,
         "botoes": [
             ["voltarcasa1", "Voltar para casa"],
             ["irtrem2", "Continuar indo até a estação de trem"]
@@ -61,7 +67,8 @@ let choices_data = {
     },
     'irtrem2': {
         "conteudo": `Você continua indo até a estação. Ao chegar lá, você vê todas pessoas se afastando, inclusive os 
-        funcionários. Quando vai à recepção da ferroviária, não tem ninguém para te atender.`,
+        funcionários. Quando vai à recepção da ferroviária, não tem ninguém para te atender.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp.gif" alt="">`,
         "botoes": [
             ["voltarcasa1", "Ir para sua casa"],
             ["irtrem3", "Ficar esperando alguém"]
@@ -70,7 +77,9 @@ let choices_data = {
     'irtrem3': {
         "conteudo": `Você fica esperando alguém chegar, mas você percebe que não há mais ninguém no local. Depois de um 
         tempo, você é surpreendido pelo barulho de sirene do carro de polícia. Antes que você pudesse fazer algo, você é 
-        abordado por dois policiais armados e apontando suas armas na sua direção. <br> - Mãos na cabeça aberração!`,
+        abordado por dois policiais armados e apontando suas armas na sua direção. <br> - Mãos na cabeça aberração!</p>
+        <img style="height: auto; width: 256px;" src="../img/policial.png" alt="">
+        <img style="height: auto; width: 256px;" src="../img/policial_arma.png" alt="">`,
         "botoes": [
             ["reagirpolicia1", "Tentar fugir"],
             ["serpreso1", "Ficar parado"]
@@ -78,8 +87,9 @@ let choices_data = {
     },
     'serpreso1': {
         "conteudo": `Você fica parado e os policiais te prendem depois de te baterem. Você não entende bem o motivo. Você 
-        é lavado a prisão e fica numa sala, sozinho, afastado de todos. 
+        é lavado a prisão e fica numa sala, sozinho, afastado de todos.</p>
         <h1>O jogo acabou, você foi preso.</h1>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_preso.gif" alt="">
         <p>Acesse os créditos abaixo ou volte ao início do jogo caso queira jogar novamente :)</p>
         <button onclick="window.location.href = '../html/creditos.html'">Créditos</button>
         <button onclick="window.location.href = '../index.html'">Menu</button>`,
@@ -88,8 +98,9 @@ let choices_data = {
     },
     'reagirpolicia1': {
         "conteudo": `Você se mexe um pouco para o lado e se prepara para correr; mas antes que possa fazer qualquer 
-        coisa <br> - Atira nessa aberração! <br> O policial atira em você, causando sua morte.
+        coisa <br> - Atira nessa aberração! <br> O policial atira em você, causando sua morte.</p>
         <h1>O jogo acabou, você morreu para um policial.</h1>
+        <img style="height: auto; width: 256px;" src="../img/gregor_lapide.png" alt="">
         <p>Acesse os créditos abaixo ou volte ao início do jogo caso queira jogar novamente :)</p>
         <button onclick="window.location.href = '../html/creditos.html'">Créditos</button>
         <button onclick="window.location.href = '../index.html'">Menu</button>`,
@@ -99,7 +110,8 @@ let choices_data = {
     'voltarcasa1': {
         "conteudo": `Você percebe que quando você volta para casa, ainda há muitas pessoas mantendo distância e sempre
         sussurrando uma para a outra. Você finalmente chega na sua casa e pelo transtorno que houve, você vai direto
-        para o seu quarto.`,
+        para o seu quarto.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_rua.png" alt="">`,
         "botoes": [
             ["ficarcasa1", "Continuar"],
         ]
@@ -107,7 +119,10 @@ let choices_data = {
     'ficarcasa1': {
         "conteudo": `Estando em sua casa, mais você se perde nos pensamentos sobre o que fazer. Quando nota, são mais de 
         sete horas e realmente perdeu o horário de trabalho. Você então escuta batidas cautelosas na porta. Você então 
-        percebe a voz de sua mãe falando "Gregor? Já são sete horas. Não tem que pegar o trem?"`,
+        percebe a voz de sua mãe falando "Gregor? Já são sete horas. Não tem que pegar o trem?"</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp_cama.png" alt="">
+        <img style="height: auto; width: 256px;" src="../img/portaquarto.png" alt="">
+        <img style="height: auto; width: 256px;" src="../img/mae.gif" alt="">`,
         "botoes": [
             ["pedirajuda1", "Falar com sua mãe sobre o seu problema"],
             ["continuar2", "- Sim, sim, obrigado, mãe, já vou levantar."],
@@ -117,7 +132,9 @@ let choices_data = {
         "conteudo": `- Mãe, aconteceu alguma coisa comigo, eu não sei explicar bem...
         <br> - Oh Deus, você está doente meu filho? Quer que eu vá chamar um médico? Abra essa porta para eu ver se é grave. 
         <br> - Não é bem uma doença, eu acho; é algo que está acontecendo no meu corpo. Eu irei abrir a porta, mas por 
-        favor, não surta.`,
+        favor, não surta.</p>
+        <img style="height: auto; width: 256px;" src="../img/gregorp.gif" alt="">
+        <img style="height: auto; width: 256px;" src="../img/mae.gif" alt="">`,
         "botoes": [
             ["continuar20", "Abrir a porta"],
         ]
@@ -125,7 +142,7 @@ let choices_data = {
     'continuar20': {
         "conteudo": `Com esse diálogo que você tem com sua mãe, as pessoas da família percebem que você está em casa. Você  
         abre a porta, então seu pai nem te estranha, mas quando você percebe sua irmã está junto de sua mãe. Assim que elas 
-        te veem, te estranham, te olham com choque.`,
+        te veem, te estranham, te olham com choque.`, // BOTAR IMAGEM AQUI E ABAIXO
         "botoes": [
             ["continuarajuda1", "- Por favor, me ajudem"],
         ]
